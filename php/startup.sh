351a8342135d35185e2197c892e6cf6f7660b643
Ankush Goyal <goyalzz@users.noreply.github.com>
2018-10-07 10:28:24 +0530

    Update StartUp
    Update Start-Up Script Set password From Env Variables

--- php/startup.sh
+++ php/startup.sh
@@ -3,11 +3,11 @@ service mysql start
 
 # Secure MariaDB with password
 mysql -h localhost -uroot -ptest mysql -e "SELECT host, user, password, plugin FROM mysql.user LIMIT 0,1;"
-mysql -h localhost -uroot -ptest mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');"
+mysql -h localhost -uroot -ptest mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');"
 mysql -h localhost -uroot -ptest mysql -e "UPDATE mysql.user SET plugin = '' WHERE user = 'root' AND host = 'localhost';"
 mysql -h localhost -uroot -ptest mysql -e "FLUSH PRIVILEGES;"
-mysql -h localhost -uroot -proot -e "GRANT ALL ON *.* to root@'%' IDENTIFIED BY 'root';"
-mysql -h localhost -uroot -proot mysql -e "FLUSH PRIVILEGES;"
+mysql -h localhost -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* to root@'%' IDENTIFIED BY 'root';"
+mysql -h localhost -uroot -p${MYSQL_ROOT_PASSWORD} mysql -e "FLUSH PRIVILEGES;"
 # mysql -h localhost -uroot -proot -e "DROP DATABASE cias;"
 # mysql -h localhost -uroot -proot -e "CREATE DATABASE IF NOT EXISTS cias;"
 # mysql -h localhost -uroot -proot cias < /var/www/html/cias.sql
